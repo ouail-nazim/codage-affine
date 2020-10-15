6a6ea2ee89a2790359e0f2778d7552ae511ba3d9
ouail-nazim <ouailnazim20@gmail.com>
2020-10-15 20:25:51 +0100

    fix the bug & app working

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -16,8 +16,9 @@ public class Controller {
     @FXML private TextField B ;
     @FXML private TextArea Message ;
     @FXML private TextArea Resultat ;
+    @FXML private Label equ ;
 
-
+    // click sur le button encryption
     public void encryption(ActionEvent actionEvent) {
         if ((A.getText().length() >= 1) &&(B.getText().length() >= 1)&&(Message.getText().length() >= 1)){
             try {
@@ -27,6 +28,7 @@ public class Controller {
                     before=Message.getText().toUpperCase();
                     after=this.encryptionMessage(before,a,b);
                     Resultat.setText(after);
+                    Message.setText("");
                 }else {
                     Resultat.setText("A doit être premier avec 26");
                 }
@@ -37,6 +39,7 @@ public class Controller {
             Resultat.setText("mal remplir");
         }
     }
+    // click sur le button decryption
     public void decryption(ActionEvent actionEvent) {
         if ((A.getText().length() >= 1) &&(B.getText().length() >= 1)&&(Message.getText().length() >= 1)){
             try {
@@ -47,6 +50,7 @@ public class Controller {
                     a=this.getFlag(a);
                     after=this.deccryptionMessage(before,a,b);
                     Resultat.setText(after.toLowerCase());
+                    Message.setText("");
                 }else {
                     Resultat.setText("A doit être premier avec 26");
                 }
@@ -57,24 +61,7 @@ public class Controller {
             Resultat.setText("mal remplir");
         }
     }
-    public boolean prem26(Integer a){
-        if (a%13 == 0 || a%2 == 0){
-            return false;
-        }
-        return true;
-    }
-    public int getFlag(Integer a){
-        int flag = 0;
-        for (int i = 0; i < 26; i++)
-        {
-            flag = (a * i) % 26;
-            if (flag == 1)
-            {
-               return i;
-            }
-        }
-        return -1;
-    }
+    // encryption the message
     public String encryptionMessage(String Msg,int a ,int b){
         String Cmsg = "";
         for (int i = 0; i < Msg.length(); i++)
@@ -88,29 +75,45 @@ public class Controller {
 
 
         }
+        String equstr="X=("+a+"*Y)+"+b+"))mod 26";
+        equ.setText("Chiffrement : "+equstr);
         return Cmsg;
     }
+    // decryption the message
     public String deccryptionMessage(String Msg,int a ,int b){
         String DCmsg = "";
         for (int i = 0; i < Msg.length(); i++)
         {
             if (Msg.charAt(i) != '#' ){
                 int Y=alphbie.indexOf(Msg.charAt(i));
-               // this is the Error
-                int X=((a*(Y-b))%26);
-                String equ=Msg.charAt(i)+"=>"+Y+" :X=("+a+"*("+Y+'-'+b+"))mod 26 => X="+X ;
-                System.out.println(equ);
-
-                //DCmsg= DCmsg+alphbie.charAt(X);
-
+                int X=Math.floorMod((a*(Y-b)), 26);
+                DCmsg= DCmsg+alphbie.charAt(X);
             }else {
-                DCmsg= DCmsg+"_";
+                DCmsg= DCmsg+" ";
             }
         }
-        //System.out.println(DCmsg);
-        //return DCmsg;
-        return "not working yet";
+        String equstr="X=("+a+"*(Y-"+b+"))mod 26";
+        equ.setText("Déchiffrement : "+equstr);
+        return DCmsg;
     }
-
-
+    // check if a is premier avec 26
+    public boolean prem26(Integer a){
+        if (a%13 == 0 || a%2 == 0){
+            return false;
+        }
+        return true;
+    }
+    // get l’inverse modulaire de a
+    public int getFlag(Integer a){
+        int flag = 0;
+        for (int i = 0; i < 26; i++)
+        {
+            flag = (a * i) % 26;
+            if (flag == 1)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
